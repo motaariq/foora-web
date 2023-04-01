35cdae31d3ee460074485378f82f16596436ea6d
motaariq <tariqkk774@gmail.com>
2023-04-01 20:56:47 +0200

    footer

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, body_might_complete_normally_nullable
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -6,7 +8,117 @@ navigatePush(context, state) {
 }
 
 Widget myLoader()=>  Center(child: Lottie.asset('assets/images/loading.json',height: 100,width: 100,frameRate: FrameRate(60)));
-
+Widget myFooter(context)=> Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.30,
+                      color: Color(0xff081528),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 120),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'FOORA-GOO',
+                                    style: TextStyle(
+                                        fontSize: 28, color: Colors.orange),
+                                  ),
+                                  Text(
+                                    'Sporting project for helping the raising Stars\n to be on the Stage ',
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey[600]),
+                                  ),
+                                  SizedBox(
+                                    height: 22,
+                                  ),
+                                  Text(
+                                    '® Copyrights. All rights reserved.foora-goo.com',
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey[600]),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Quick Links',
+                                    style: TextStyle(
+                                        fontSize: 28, color: Colors.white),
+                                  ),
+                                  Text('Home',
+                                      style:
+                                          TextStyle(color: Colors.grey[600])),
+                                  Text('About',
+                                      style:
+                                          TextStyle(color: Colors.grey[600])),
+                                  Text('Get started',
+                                      style:
+                                          TextStyle(color: Colors.grey[600])),
+                                  Text('FAQ',
+                                      style: TextStyle(color: Colors.grey[600]))
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'New Sletter',
+                                    style: TextStyle(
+                                        fontSize: 28, color: Colors.white),
+                                  ),
+                                  Text(
+                                      'You can contact us by \n sending us your feedback ',
+                                      style: TextStyle(color: Colors.white)),
+                                      SizedBox(height: 7,),
+                                  Container(
+                                    height: 40,
+                                    width: 350,
+                                    color: Colors.white,
+                                    child: TextFormField(
+                                      validator: (value) {
+                                        if (value!.isEmpty) {
+                                          return "email mustn't be empty";
+                                        }
+                                      },
+                                      cursorColor: Colors.blue,
+                                      cursorHeight: 25,
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontFamily: 'jannah',
+                                          fontSize: 14),
+                                      keyboardType: TextInputType.name,
+                                      decoration: InputDecoration(
+                                          
+                                          suffixIcon: Container(color: Colors.orange,child: Icon(Icons.send,color: Colors.white,)),
+                                          contentPadding: EdgeInsets.only(
+                                              bottom: 15, left: 8, top: 5),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderSide: const BorderSide(
+                                                color: Colors.blue),
+                                          ),
+                                          enabledBorder: OutlineInputBorder(
+                                            borderSide:
+                                                BorderSide(color: Colors.grey),
+                                          )),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
 
 navigateTo(context, state) {
   Navigator.of(context).pushNamed(state);
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -11,95 +11,148 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover, image: AssetImage('assets/images/home.jpg')),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 50),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+        backgroundColor: Color(0xff081528),
+        appBar: AppBar(
+          toolbarHeight: 75,
+          backgroundColor: primaryColor,
+          title: Row(
             children: [
               Text(
-                '[',
-                style: TextStyle(fontSize: 300, color: Colors.orange),
+                'Foora-Goo',
+                style: TextStyle(
+                    color: Colors.orange, fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'We Make The Way Easy',
-                      style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'select your team , select your place and time',
-                      style: TextStyle(
-                        fontSize: 28,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 20,),
-                    Row(
-                      children: [
-                        InkWell(
-                          onTap:(){
-                           navigateTo(context, LoginScreen.route);
-                          },
-                          child: Container(
-                            width: 140,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.orange,
-                              borderRadius: BorderRadius.circular(5)
-                            ),
-                            child: Center(
-                              child: Text('Login',style: TextStyle(
-                                color: primaryColor,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold
-                              ),),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 10,),
-                        InkWell(
-                          onTap: (){
-
-                          },
-                          child: Container(
-                            width: 140,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.circular(5)
-                            ),
-                            child: Center(
-                              child: Text('Sign Up',style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold
-                              ),),
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              )
+              SizedBox(
+                width: 80,
+              ),
+              Text('Home', style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 25,
+              ),
+              Text('Games', style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 25,
+              ),
+              Text('Stadiums', style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 25,
+              ),
+              Text('Captins', style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 25,
+              ),
+              Text('About', style: TextStyle(color: Colors.white))
             ],
           ),
         ),
-      ),
-    );
+        body: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.70,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/home.jpg')),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 50),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '[',
+                            style:
+                                TextStyle(fontSize: 300, color: Colors.orange),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'We Make The Way Easy',
+                                  style: TextStyle(
+                                      fontSize: 35,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'select your team , select your place and time',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        navigateTo(context, LoginScreen.route);
+                                      },
+                                      child: Container(
+                                        width: 140,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.orange,
+                                            borderRadius:
+                                                BorderRadius.circular(5)),
+                                        child: Center(
+                                          child: Text(
+                                            'Login',
+                                            style: TextStyle(
+                                                color: primaryColor,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        width: 140,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey,
+                                            borderRadius:
+                                                BorderRadius.circular(5)),
+                                        child: Center(
+                                          child: Text(
+                                            'Sign Up',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  myFooter(context)
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
